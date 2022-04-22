983e31b094c05c6f0c6d9f87bd93a709835f8932
Francella Aguilar <francellaaguilar@Francellas-MBP.attlocal.net>
2022-04-22 16:02:23 -0400

    adding .env file and ran schema.sql

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const routes = require('./routes');
-
+const express = require("express");
+const routes = require("./routes");
+const sequelize = require("./config/connection");
 // import sequelize connection
-const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,6 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
