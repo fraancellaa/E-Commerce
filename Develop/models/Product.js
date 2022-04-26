df300ef0aab53b6ee1bf4d15fbf1e52fb8061b68
Francella Aguilar <francellaaguilar@Francellas-MBP.attlocal.net>
2022-04-26 18:53:24 -0400

    fixing product model, autoIncrement camelcase

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -13,14 +13,14 @@ Product.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      auto_increment: true
+      autoIncrement: true
     },
     product_name: {
       type: DataTypes.STRING,
       allowNull: false
     },
     price: {
-      type: DataTypes.DECIMAL,
+      type: DataTypes.DECIMAL(10,2),
       allowNull: false,
       // validate if the input is indeed a decimal
       validate: {
